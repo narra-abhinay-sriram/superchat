03679dfa2bf293a0e08a99f789419730e1292722
karun-ch <170010020@iitdh.ac.in>
2024-12-24 16:54:13 +0530

    Changes in Signup Page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -5,6 +5,8 @@ import { Login_Api, Signup_api } from "../Utils/Apis";
 import { useDispatch } from "react-redux";
 import { login } from "../ReduxStateManagement/authslice";
 import { GoogleLogin } from "@react-oauth/google";
+import LandingPage__Left_Container from "../LandingPage_components/LandingPage__Left_Container";
+
 
 export default function SignupPage() {
   const [passwordVisible, setPasswordVisible] = useState(false);
@@ -58,112 +60,140 @@ export default function SignupPage() {
   };
 
   return (
-    <div className="w-full max-w-sm sm:max-w-md mx-auto mt-16 p-6 border rounded-lg shadow-md">
-      <form onSubmit={handleloginsubmit}>
-        <h1 className="text-2xl font-semibold mb-6 text-center">
-          {loginhandle ? "Login" : "Sign Up"}
-        </h1>
-
-        <label className="block text-sm font-medium mb-2">Email</label>
-        <input
-          type="text"
-          placeholder="chris@gmail.com"
-          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          id="email"
-          onChange={(e) =>
-            setformdata({ ...formdata, [e.target.id]: e.target.value })
-          }
-          disabled={loading}
-        />
-
-        <label className="block text-sm font-medium mb-2">Password</label>
-        <div className="relative">
-          <input
-            type={passwordVisible ? "text" : "password"}
-            placeholder="chris@1234"
-            className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            id="password"
-            onChange={(e) =>
-              setformdata({ ...formdata, [e.target.id]: e.target.value })
-            }
-            disabled={loading}
-          />
-          <button
-            type="button"
-            onClick={togglePasswordVisibility}
-            className="absolute inset-y-0 right-3 flex items-center"
-            disabled={loading}
+    <div className="flex min-h-screen w-full">  
+      {/* Left Section */}
+      <div className="hidden lg:flex flex-col w-1/2 bg-slate-50 relative">
+        {/* Header with Logo */}
+        <div className="absolute top-4 left-4">
+          <span
+            className="sm:text-[22px] text-xl font-inter font-semibold bg-clip-text text-transparent cursor-pointer"
+            style={{
+              backgroundImage: "linear-gradient(to right, #6F036C, #FF6F61E5)",
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
           >
-            {passwordVisible ? (
-              <FiEye className="w-5 h-5 text-gray-500 mb-3" />
-            ) : (
-              <FiEyeOff className="w-5 h-5 text-gray-500 mb-3" />
-            )}
-          </button>
+            Superchat LLC
+          </span>
         </div>
+  
+        {/* Left Container */}
+        <div className="col-span-1 sm:col-span-2 justify-center item-center mt-20" >
+          <LandingPage__Left_Container />
+        </div>
+      </div>
+  
+      {/* Right Section changed*/}
+<div className="w-1/2 h-screen flex items-center justify-center bg-white  p-20">
+  <form
+    onSubmit={handleloginsubmit}
+    className="w-full h-full flex flex-col justify-center px-6"
+  >
+    <h1 className="text-2xl font-semibold mb-6 text-center">
+      {loginhandle ? "Login" : "Sign Up"}
+    </h1>
 
-        {error && (
-          <p className="text-red-700 text-center p-2 m-2">{error}</p>
+    <label className="block text-sm font-medium mb-2">Email</label>
+    <input
+      type="text"
+      placeholder="chris@gmail.com"
+      className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+      id="email"
+      onChange={(e) =>
+        setformdata({ ...formdata, [e.target.id]: e.target.value })
+      }
+      disabled={loading}
+    />
+
+    <label className="block text-sm font-medium mb-2">Password</label>
+    <div className="relative w-full">
+      <input
+        type={passwordVisible ? "text" : "password"}
+        placeholder="chris@1234"
+        className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        id="password"
+        onChange={(e) =>
+          setformdata({ ...formdata, [e.target.id]: e.target.value })
+        }
+        disabled={loading}
+      />
+      <button
+        type="button"
+        onClick={togglePasswordVisibility}
+        className="absolute inset-y-0 right-3 flex items-center"
+        disabled={loading}
+      >
+        {passwordVisible ? (
+          <FiEye className="w-5 h-5 text-gray-500 mb-3" />
+        ) : (
+          <FiEyeOff className="w-5 h-5 text-gray-500 mb-3" />
         )}
-
-        <button
-          type="submit"
-          className="w-full py-2 text-white bg-black rounded-lg hover:bg-gray-800"
-          disabled={loading}
-        >
-          {loginhandle ? "Login" : "Sign Up"}
-        </button>
-
-        <p className="text-center text-gray-600 my-4">Or continue with</p>
-       {/* <div className="flex justify-center gap-4">
-          <button
-            disabled={loading}
-            className="flex items-center border border-gray-300 px-4 py-2 rounded-lg hover:bg-gray-50"
-          >
-            <img
-              src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-              alt="Google"
-              className="w-5 h-5 mr-2"
-            />
-            Google
-          </button>
-          <button
-            disabled={loading}
-            className="flex items-center border border-gray-300 px-4 py-2 rounded-lg hover:bg-gray-50"
-          >
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
-              alt="Apple"
-              className="w-5 h-5 mr-2"
-            />
-            Apple
-          </button>
-        </div> */}
-
-        <p className="text-center mt-4 text-sm flex flex-col sm:flex-row gap-2 justify-center">
-          {loginhandle ? (
-            <>
-              Don{"'"}t have an account?{" "}
-              <span
-                onClick={() => setloginhandle(!loginhandle)}
-                className="text-indigo-600 hover:underline cursor-pointer"
-              >
-                Sign Up
-              </span>
-            </>
-          ) : (
-            <>
-              Already have an account?{" "}
-              <span
-                onClick={() => setloginhandle(!loginhandle)}
-                className="text-indigo-600 hover:underline cursor-pointer"
-              >
-                Log In
-              </span>
-            </>
-          )}
-        </p>
-      </form>
+      </button>
     </div>
+
+    {error && (
+      <p className="text-red-700 text-center p-2 m-2">{error}</p>
+    )}
+
+    <button
+      type="submit"
+      className="w-full py-2 text-white bg-black rounded-lg hover:bg-gray-800"
+      disabled={loading}
+    >
+      {loginhandle ? "Login" : "Sign Up"}
+    </button>
+
+    <p className="text-center text-gray-600 my-4">Or continue with</p>
+    <div className="flex justify-center gap-4">
+      <button
+        disabled={loading}
+        className="flex items-center border border-gray-300 px-4 py-2 rounded-lg hover:bg-gray-50"
+      >
+        <img
+          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+          alt="Google"
+          className="w-5 h-5 mr-2"
+        />
+        Google
+      </button>
+      <button
+        disabled={loading}
+        className="flex items-center border border-gray-300 px-4 py-2 rounded-lg hover:bg-gray-50"
+      >
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
+          alt="Apple"
+          className="w-5 h-5 mr-2"
+        />
+        Apple
+      </button>
+    </div>
+
+    <p className="text-center mt-4 text-sm flex flex-col sm:flex-row gap-2 justify-center">
+      {loginhandle ? (
+        <>
+          Don{"'"}t have an account?{" "}
+          <span
+            onClick={() => setloginhandle(!loginhandle)}
+            className="text-indigo-600 hover:underline cursor-pointer"
+          >
+            Sign Up
+          </span>
+        </>
+      ) : (
+        <>
+          Already have an account?{" "}
+          <span
+            onClick={() => setloginhandle(!loginhandle)}
+            className="text-indigo-600 hover:underline cursor-pointer"
+          >
+            Log In
+          </span>
+        </>
+      )}
+    </p>
+  </form>
+</div>
+     </div>
   );
 }
